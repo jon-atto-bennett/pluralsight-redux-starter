795b3f20bcb0920b7d4f324f5e959741ae636481
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-07-17 12:19:43 +1200

    added courselist and courselistrow components

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware} from 'redux'
 import rootReducer from '../reducers/'
 import reduxImmutableState from 'redux-immutable-state-invariant'
+import thunk from 'redux-thunk'
 
 export default function configureStore (initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableState())
+    applyMiddleware(thunk, reduxImmutableState())
   )
 }
